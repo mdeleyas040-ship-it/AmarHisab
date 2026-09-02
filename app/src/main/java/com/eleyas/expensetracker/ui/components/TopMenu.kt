5d89fac347c787768717dfc5e843a89957ea0680
mdeleyas040-ship-it <mdeleyas040@gmail.com>
2026-09-02 14:02:10 +0500

    Fix Help menu state persistence

--- app/src/main/java/com/eleyas/expensetracker/ui/components/TopMenu.kt
+++ app/src/main/java/com/eleyas/expensetracker/ui/components/TopMenu.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -31,7 +32,7 @@ fun AmarHisabTopMenu(
     onShoppingList: () -> Unit,
     onSettings: () -> Unit
 ) {
-    var showHelp by mutableStateOf(false)
+    var showHelp by remember { mutableStateOf(false) }
 
     DropdownMenu(
         expanded = expanded,
