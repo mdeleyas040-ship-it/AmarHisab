3c95bd59e7f517bf54549e9a543839dc30866b1a
mdeleyas040-ship-it <mdeleyas040@gmail.com>
2026-09-19 01:23:56 +0500

    Preserve Voice Note and exchange rate in backup/cloud transaction data

--- app/src/main/java/com/eleyas/expensetracker/util/StorageUtils.kt
+++ app/src/main/java/com/eleyas/expensetracker/util/StorageUtils.kt
@@ -281,6 +281,7 @@ fun saveTransactionToFirestore(
                 "receiptImage" to (
                         transaction.receiptImage ?: ""
                         ),
+                "audioMemoPath" to (transaction.audioMemoPath ?: ""),
                 "walletId" to transaction.walletId,
                 "addedByUid" to (
                         transaction.addedByUid ?: ""
@@ -291,7 +292,8 @@ fun saveTransactionToFirestore(
                 "exchangeRateUsed" to (
                         transaction.exchangeRateUsed
                             ?: -1.0
-                        )
+                        ),
+                "audioMemoPath" to (transaction.audioMemoPath ?: "")
             )
         )
         .addOnSuccessListener {
@@ -1267,6 +1269,14 @@ fun buildBackupJson(
                 put(
                     "walletId",
                     transaction.walletId
+                )
+                put(
+                    "exchangeRateUsed",
+                    transaction.exchangeRateUsed ?: -1.0
+                )
+                put(
+                    "audioMemoPath",
+                    transaction.audioMemoPath ?: ""
                 )
                 put(
                     "addedByUid",
