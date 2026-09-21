01641cddbe21e6d1659bf5521aff213b71537c76
mdeleyas040-ship-it <mdeleyas040@gmail.com>
2026-09-21 15:38:14 +0500

    chore: clean duty roster imports

--- app/src/main/java/com/eleyas/expensetracker/dutyroster/DutyRosterScreen.kt
+++ app/src/main/java/com/eleyas/expensetracker/dutyroster/DutyRosterScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.border
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -30,7 +29,6 @@ import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
