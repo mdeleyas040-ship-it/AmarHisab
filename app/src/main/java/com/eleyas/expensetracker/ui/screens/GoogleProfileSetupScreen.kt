8e2ccba8434969418c367b6dc0c5a19d2eeb3436
mdeleyas040-ship-it <mdeleyas040@gmail.com>
2026-09-22 21:15:11 +0500

    Polish profile setup API usage on dev

--- app/src/main/java/com/eleyas/expensetracker/ui/screens/GoogleProfileSetupScreen.kt
+++ app/src/main/java/com/eleyas/expensetracker/ui/screens/GoogleProfileSetupScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GoogleProfileSetupScreen(
     onComplete: () -> Unit,
